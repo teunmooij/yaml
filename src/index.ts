2073478e0c12ed05cfcd42c89eb2d54fbb273b2d
teunmooij <tmooij@infinitaslearning.com>
2023-06-28 00:06:51 +0200

    merge and set

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as core from '@actions/core';
-import path from 'path';
+import { join } from 'path';
 import fs from 'fs/promises';
 import * as yaml from 'yaml';
+import * as jp from 'jsonpath';
+import merge from 'deepmerge';
 
 const readFromFile = async (fromPath: string) => {
   const content = await fs.readFile(fromPath, 'utf-8');
@@ -10,33 +12,41 @@ const readFromFile = async (fromPath: string) => {
 
 const readInput = async () => {
   const fromFile = core.getInput('from-file');
-  const data = core.getInput('data');
+  const inputData = core.getInput('data');
   const toFile = core.getInput('to-file');
-  const modiciations = core.getInput('modifications');
+  const mergeData = core.getInput('merge');
+  const setData = core.getInput('set');
 
   if (fromFile || toFile) {
     if (!process.env.GITHUB_WORKSPACE) throw new Error('Please checkout your repository first (see README)');
   }
 
-  if (fromFile && data) throw new Error('Please provide either from-file or data, not both');
-  if (!fromFile && !data) throw new Error('Please provide either from-file or data');
+  if (fromFile && inputData) throw new Error('Please provide either from-file or data, not both');
+  if (!fromFile && !inputData) throw new Error('Please provide either from-file or data');
 
   return {
-    inputData: data ? JSON.parse(data) : await readFromFile(path.join(process.env.GITHUB_WORKSPACE!, fromFile!)),
-    modifications: modiciations ? JSON.parse(modiciations) : null,
-    toFile: toFile ? path.join(process.env.GITHUB_WORKSPACE!, toFile) : null,
+    data: inputData ? JSON.parse(inputData) : await readFromFile(join(process.env.GITHUB_WORKSPACE!, fromFile!)),
+    merge: mergeData ? JSON.parse(mergeData) : {},
+    set: setData ? JSON.parse(setData) : {},
+    toFile: toFile ? join(process.env.GITHUB_WORKSPACE!, toFile) : null,
   };
 };
 
 const run = async (): Promise<void> => {
   try {
-    const { inputData, toFile } = await readInput();
+    const input = await readInput();
+    const { data } = input;
 
-    //TODO: modifications
-    const data = inputData;
+    for (const [path, value] of Object.entries(input.merge)) {
+      jp.apply(data, path, current => merge(current, value as any));
+    }
 
-    if (toFile) {
-      fs.writeFile(toFile, yaml.stringify(data));
+    for (const [path, value] of Object.entries(input.set)) {
+      jp.apply(data, path, () => value);
+    }
+
+    if (input.toFile) {
+      fs.writeFile(input.toFile, yaml.stringify(data));
     }
 
     core.setOutput('data', data);
